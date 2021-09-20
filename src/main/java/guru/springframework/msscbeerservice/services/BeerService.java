7b395cb348eda78ec4b53e03e5e3fad7253265b2
Khalil SLEIMI <khalil.sleimi@vneuron.com>
2021-09-20 18:04:09 +0100

    Add methods for controller

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    public BeerDto getById(UUID beerId);
+
+    public BeerDto saveNewBeer(BeerDto beerDto);
+
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+}
